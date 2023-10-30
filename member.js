ddda653e6bd9ecb6fad1264e5619d538c66aa182
Vidyaa <mailvidyaa@gmail.com>
2023-10-30 13:15:27 +0000

    add member.js

---
+++ member.js
@@ -0,0 +1,34 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkills2 = document.getElementById("memberSkills2");
+    var memberSkills3 = document.getElementById("memberSkills3");
+    var memberSkills4 = document.getElementById("memberSkills4");
+    var memberSkills5 = document.getElementById("memberSkills5");
+    var memberSkills6 = document.getElementById("memberSkills6");
+    var memberSkills7 = document.getElementById("memberSkills7");
+    var memberSkills8 = document.getElementById("memberSkills8");
+    var memberSkills9 = document.getElementById("memberSkills9");
+    var memberSkills10 = document.getElementById("memberSkills10");
+    var memberSkills11 = document.getElementById("memberSkills11");
+    var memberSkills12 = document.getElementById("memberSkills12");
+    var memberSkills13 = document.getElementById("memberSkills13");
+    var memberSkills14 = document.getElementById("memberSkills14");
+    memberSkills.innerHTML = "Skills:";
+    memberSkills2.innerHTML = "Skills:";
+    memberSkills3.innerHTML = "Skills:";
+    memberSkills4.innerHTML = "Skills:";
+    memberSkills5.innerHTML = "Skills:";
+    memberSkills6.innerHTML = "Skills:";
+    memberSkills7.innerHTML = "Skills:";
+    memberSkills8.innerHTML = "Skills:";
+    memberSkills9.innerHTML = "Skills:";
+    memberSkills10.innerHTML = "Skills:";
+    memberSkills11.innerHTML = "Skills:";
+    memberSkills12.innerHTML = "Skills:";
+    memberSkills13.innerHTML = "Skills:";
+    memberSkills14.innerHTML = "Skills:";
+    member.innerHTML = "Member";
+}
+
+
